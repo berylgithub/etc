6f9e6d8c622491c53a77e97a567cfd02ed1ea73e
berylramadhian <berylramadhian@gmail.com>
2025-05-21 12:13:54 +0200

    more example for perfoptim: psd matrix from gaussian kernel

--- perfoptim.jl
+++ perfoptim.jl
@@ -94,3 +94,59 @@ A = rand(1000, 1000)
 F = zeros(size(A));
 @btime apply_filter($A)
 @btime apply_filter!($F, $A)  #no allocation, much faster
+
+"""
+good ol' Gaussian Kernel. Maps 2 vectors → scalar
+"""
+function gaussian_kernel(u, v, σ)
+    return exp(-norm(u-v)^2/2*σ^2)
+end
+
+# no vector allocation inside, since it's a reduce operation, just allocate a scalar
+function gaussian_kernel_noalloc(u, v, σ)
+    r = 0.
+    for i ∈ eachindex(u)
+        r += (u[i] - v[i])^2
+    end
+    return exp(-r/2*σ^2)
+end
+
+u = ones(100_000_000); v = zeros(100_000_000); σ = 1.;
+@btime gaussian_kernel($u, $v, $σ)
+@btime gaussian_kernel_noalloc($u, $v, $σ) # no allocation, much faster
+
+
+"""
+P.s.d matrix from Gaussian Kernel calls
+"""
+function generate_psdmatrix(A, B, σ)
+    m = size(A,1); n = size(B,1);
+    R = zeros(m,n)
+    for i ∈ axes(A,1)
+        for j ∈ axes(B,1)
+            R[i,j] = gaussian_kernel_noalloc(A[i,:], B[j,:], σ)
+        end
+    end
+    return R
+end
+
+function generate_psdmatrix!(R, A, B, σ)
+    for i ∈ axes(A,1)
+        for j ∈ axes(B,1)
+            R[i,j] = gaussian_kernel_noalloc(@view(A[i,:]), @view(B[j,:]), σ) # @view is the key, it refers to the memory location rather than reallocating vectors
+        end
+    end
+end
+
+function generate_psdmatrix_threadsx(A, B, σ) # try ThreadsX map
+    I = axes(A,1); J = axes(B, 1);
+    IJ = Iterators.product(I, J) # product of two vectors of indices
+    return ThreadsX.map(ij -> gaussian_kernel_noalloc(@view(A[ij[1],:]), @view(B[ij[2],:]), σ), IJ) # this will allocate, trade off between speed and memory
+end
+
+
+A = rand(10_000, 100); σ = 1.; 
+R = zeros(size(A,1), size(A,1));
+@btime generate_psdmatrix($A, $A, $σ)
+@btime generate_psdmatrix!($R, $A, $A, $σ) # no allocation, faster
+@btime generate_psdmatrix_threadsx($A, $A, $σ) # with ThreadsX, tradeoff between speed and memory, should be faster with higher number of threads (tested with 64 vs 1 threadnums)
